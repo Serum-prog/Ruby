e799fe52da81ddf0882de8c0bbe9dc24c5603a57
Serum-prog <73681718+Serum-prog@users.noreply.github.com>
2020-10-30 13:26:37 +0700

    Update main.rb

--- lab1/lib/main.rb
+++ lab1/lib/main.rb
@@ -1,2 +1,19 @@
+load 'lib/input.rb'
+load 'lib/converter.rb'
+
+class Main
+  def main
+    input = Input.new
+    value = input.read_value
+    scale = input.read_scale
+    new_scale = input.read.scale
+
+    conv = Converter.new
+    result = conv.convert(value, scale, new_scale)
+
+    puts "#{vaue}#{scale} = #{result}#{new_scale}"
+  end
+end
+
 a = Main.new
 a.main
